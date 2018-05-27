c939fa5b8ec012c53807b8a8f32744ffd5e2a233
mlt <miltozz@gmail.com>
2018-05-27 18:32:17 +0300

    Version 1

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
-const addTasks = document.querySelector('.add-tasks');
-const tasksList = document.querySelector('.tasks');
-const tasks = [];
+(function todo() {
+  const addTasks = document.querySelector(".add-tasks");
+  const tasksList = document.querySelector(".tasks");
+  const tasks = [];
 
+  function populate() {
+    tasksList.innerHTML = tasks
+      .map(task => {
+        let status = task.done ? "done" : " ";
+        return `
+        <li>
+        <input type="checkbox">
+        <label class=${status}>${task.text}</label>
+        <span class="del">x</span>
+        </li>`;
+      })
+      .join("");
+  }
 
-function addTask(e){
+  function addTask(e) {
     e.preventDefault();
-    console.log(e.target);
-    const taskText = this.querySelector('[type=text]').value;
-    console.log(taskText);
+    const taskText = this.querySelector("[type=text]").value;
     const task = {
-        text:taskText,
-        done:false
+      text: taskText,
+      done: false
     };
-    console.log(task);
 
     tasks.push(task);
-    console.log(tasks);
     this.reset();
-    
-    
-    
+    populate();
+  }
 
-    
-}
+  function handleClick(e) {
+    if (!e.target.matches("span") && !e.target.matches("label")) {
+      return;
+    }
 
-addTasks.addEventListener('submit', addTask);
+    if (e.target.classList.contains("del")) {
+      let labelText = e.target.previousElementSibling.innerHTML;
+      let index = false;
+
+      for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].text === labelText) {
+          index = i;
+        }
+      }
+
+      tasks.splice(index, 1);
+      populate();
+    }
+
+    if (e.target.matches("label")) {
+      e.target.classList.toggle("done");
+      let labelText = e.target.innerHTML;
+
+      tasks.forEach(task => {
+        if (task.text === labelText) {
+          task.done = !task.done;
+        }
+      });
+    }
+  }
+
+  addTasks.addEventListener("submit", addTask);
+  tasksList.addEventListener("click", handleClick);
+})();
